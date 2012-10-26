92ab24476b648130712cbb23260c5d2bce721f94
James Chambers <james@jameschambers.com>
2012-10-25 21:31:54 -0500

    Initial Commit
    Publishing article and code

---
+++ MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private int _myCounter = 0;
+
+        public HomeController()
+        {
+            MvcApplication.ConstructorCallCount++;
+        }
+
+        public ActionResult ExecutionCount()
+        {
+            return View(++_myCounter);
+        }
+        
+        public PartialViewResult ExecutionCountAsPartial()
+        {
+            return PartialView(++_myCounter);
+        }
+
+        [OutputCache(NoStore=true, Duration=1)]
+        public PartialViewResult ExecutionCountAsPartialViaAjax()
+        {
+            return PartialView("ExecutionCountAsPartial", ++_myCounter);
+        }
+
+        [SimpleActionFilter]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            Debug.WriteLine("Action (method) executing");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MvcApplication3/SimpleActionFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3
+{
+    public class SimpleActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("OnActionExecuted");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("OnActionExecuting");
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Debug.WriteLine("OnResultExecuted");
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Debug.WriteLine("OnResultExecuting");
+        }
+    }
+}
